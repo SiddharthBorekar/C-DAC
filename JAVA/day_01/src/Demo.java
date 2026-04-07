8aec2ec2b2a4bfa889fe9d344b0f81bdb2ba9d7e
Siddhant Borekar <siddhantborekar11@gmail.com>
2026-04-07 19:02:08 +0530

    update project

--- JAVA/day_01/src/Demo.java
+++ JAVA/day_01/src/Demo.java
@@ -2,7 +2,35 @@ public class Demo{
 	public static void main(String[] args) {
 		byte b = 127; //256
 		int n = 128;
-		b = (byte) n;
+		b = (byte) n;  	//Explict casting
 		System.out.println(b);
+		n = b; 			//implict casting
+
+		byte b1 = 10;
+		byte b2 = 20;
+		int i = b1 * b2;
+		System.out.println(i);
+
+		float f = 10.252f;
+		float avg = (float) 5/2;
+		System.out.println("Avg = "+avg);
+		
+//		n = n+1;
+//		n++;
+//		n += 1;X
+
+//		System.out.println(10/0);   	//Infinity
+		System.out.println(0.0/0.0);	//NAN
+		
+		int a = 0b101;
+		System.out.println(a);
+		
+		
+		int c = 11;
+		int d = ++c;
+		System.out.println(c+ " " +d);
+		
+		double no = 1_00_000;
+		System.out.println(no);
 	}
 }
