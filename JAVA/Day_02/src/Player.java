bb8b8483e9ce9a10fc8fd897abbb10e2a8f8e866
Siddhant Borekar <siddhantborekar11@gmail.com>
2026-04-08 17:20:06 +0530

    Update

---
+++ JAVA/Day_02/src/Player.java
@@ -0,0 +1,20 @@
+public class Player {
+	private String PlayerName;
+	private String PlayerCountry;
+	
+	public Player() {
+		//this ("Sachin");
+		PlayerCountry = "India";
+	}
+	public Player (String PlayerName) {
+		this();
+		this .PlayerName = PlayerName;
+	}
+	public String toString() {
+		return "Player [PlayerName = "+PlayerName+" "+PlayerCountry+"]";
+	}
+	public static void main(String[] args) {
+		Player p = new Player("Virat");
+		System.out.println(p);
+	}
+}
