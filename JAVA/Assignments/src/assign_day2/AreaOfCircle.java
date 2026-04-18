4e323cab18ebcdb902fbe1804637532f2a12f975
Siddhant Borekar <siddhantborekar11@gmail.com>
2026-04-19 03:37:39 +0530

    update

---
+++ JAVA/Assignments/src/assign_day2/AreaOfCircle.java
@@ -0,0 +1,26 @@
+package assign_day2;
+
+import java.util.Scanner;
+
+public class AreaOfCircle {
+	private int radius;
+	private double area;
+	
+	public void init() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter a radius");
+		radius = s.nextInt();
+	}
+	public void calc() {
+		area = 3.14*(radius*radius);
+	}
+	public void display() {
+		System.out.println("Area of Circle is :"+area);	}
+		
+	public static void main(String[] args) {
+		AreaOfCircle ac = new AreaOfCircle();
+		ac.init();
+		ac.calc();
+		ac.display();
+	}
+}
