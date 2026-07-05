3a834ee53a51b985642767459e9a012a0068094a
Siddhant Borekar <siddhantborekar11@gmail.com>
2026-07-05 10:03:04 +0530

    Update

--- Web_Java/HibernateCRUD/src/main/java/com/app/entity/User.java
+++ Web_Java/HibernateCRUD/src/main/java/com/app/entity/User.java
@@ -1,23 +1,36 @@
-package com.app.model;
+package com.app.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
+
+	@Column(name = "name", nullable = false, length = 100)
 	private String name;
+
+	@Column(name = "email", nullable = false, length = 150)
 	private String email;
 
 	public User() {
-		super();
 	}
 
 	public User(int id, String name, String email) {
-		super();
 		this.id = id;
 		this.name = name;
 		this.email = email;
 	}
 
 	public User(String name, String email) {
-		super();
 		this.name = name;
 		this.email = email;
 	}
