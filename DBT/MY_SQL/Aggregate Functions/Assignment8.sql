8ded340985488e60b3ff9f80ca9ac30fea69ce08
Siddhant Borekar <siddhantborekar11@gmail.com>
2026-08-30 14:46:33 +0530

    Update

--- DBT/MY_SQL/Aggregate Functions/Assignment8.sql
+++ DBT/MY_SQL/Aggregate Functions/Assignment8.sql
@@ -12,14 +12,40 @@ select count(*) from student where month(dob) = 7;
 
 -- 5. Display studentID and count the student who are having more than two phones.
 
+
 -- 6. Count unique universities from student_qualifications table.
+
 
 -- 7. Display the university name and the count of those students who have done ‘BE’
 
+
 -- 8. Count how many students has done ‘BE’.
+
 
 -- 9. Count how many students has not done ‘BE’.
 
--- 10. Find the maximum marks student got in ‘BE’.
 
+-- 10. Find the maximum marks student got in ‘BE’.
+select max(marks) from student_qualifications where name = "BE";
+
+-- 11. Find the minimum marks student got in ‘BE’.
+
+
+-- 12. Count how many course_batches have started on ’2016-02-01’.
+
+
+-- 13. Count the number of students who have more than 60% in ‘BE’.
+
+
+-- 14. Count the number of students who have more than 60% in ‘BE’ and done from ‘Harvard university’.
+
+
+-- 15. Count number of courses.
+
+
+-- 16. Count how many distinct universities from student_qualifications table.
+select count(Distinct university) from student_qualifications ;
+
+-- 17. Find the maximum marks any student has got in “BE”.
+select max(marks) from student_qualifications where name = "BE";
 
---
+++ DBT/MY_SQL/JOINS/Assingment 8.sql
@@ -0,0 +1,20 @@
+-- 1. Display all student and with their address from student and student_address tables.
+select s.* , sa.* from student s join student_address sa on s.id = sa.studentID;
+
+-- 2. Display (namefirst, namelast, emailID, and student_qualification details) from student and student_qualification relations.
+select s.namefirst,namelast,emailid , sq.* from student s join student_qualifications sq on s.id = sq.studentid;
+
+-- 3. Display (namefirst, namelast, emailID, college, and university) who have studied in 'Yale University'. (Use student, and student_qualification relation)
+select s.namefirst,namelast,emailid ,sq.college,university from student s join student_qualifications sq on s.id = sq.studentid where university = "Yale University";
+
+-- 4. Display all student details his phone details and his qualification details. (Use student, student_phone and student_qualification relation)
+ select s.* ,sp.* , sq.* from student s join student_phone sp join student_qualifications sq on s.id = sp.studentid = sq.studentid;
+
+-- 5.Display (studentID, namefirst, namelast, name, college, university, and marks) whose name is ‘BE’.(Use student, and student_qualification relation)
+select s.ID,namefirst,namelast,name,college,university from student s join student_qualifications sq on s.id = sq.studentid;
+
+-- 6. Display the module name and the duration of the module for the batch “Batch1”.
+
+
+-- 7. Display student information along with his batch details who have joined in “Batch1”.
+
