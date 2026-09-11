4f527d0c99a1a07f4af54efb72ac851b9105b465
Siddhant Borekar <siddhantborekar11@gmail.com>
2026-09-11 23:52:46 +0530

    Update

--- DBT/MY_SQL/JOINS/Assingment 8.sql
+++ DBT/MY_SQL/JOINS/Assingment 8.sql
@@ -10,7 +10,6 @@ select s.namefirst,namelast,emailid ,sq.college,university from student s join s
 -- 4. Display all student details his phone details and his qualification details. (Use student, student_phone and student_qualification relation)
  select s.* ,sp.* , sq.* from student s join student_phone sp join student_qualifications sq on s.id = sp.studentid = sq.studentid;
 
-
 -- 5.Display (studentID, namefirst, namelast, name, college, university, and marks) whose name is ‘BE’.(Use student, and student_qualification relation)
 select s.ID,namefirst,namelast,name,college,university from student s join student_qualifications sq on s.id = sq.studentid;
 
